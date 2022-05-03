946756d82dec78e4937d2507540b5b1ae94d81ca
Elijah Moore <101022928+condbess@users.noreply.github.com>
2022-05-03 06:25:48 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,41 @@
+const { Client, Intents } = require("discord.js");
+//fetch will be used to call the API
+const fetch = new Client({ intents: [
+  Intents.FLAGS.NODES,
+  Intents.FLAG.NODE_RES
+]});
+const client = new Client({ intents: [
+  Intents.FLAGS.GUILDS,
+  Intents.FLAGS.GUILD_MESSAGES
+]});
+
+//this function fetches and returns the quote from the provided URL
+function getQuote() {
+  return fetch("https://zenquotes.io/api/random")
+    //after the info is fetched, 
+    //asynchronous call (which allows the function to wait until the data is retrieved "then" it's going to do smth w/ that data)
+    .then(res => {//'res' callback function
+      return res.json()
+    })
+    .then(data => {//custom formating
+      return data[0]["q"] + " -" + data[0]["a"]
+      //q is for quote & a is for author
+    })
+}; 
+
+client.on("ready", () => {
+  console.log('Logged in as ' + client.user.tag + '!')
+});
+
+client.on("message", msg => {
+  //check if the message is from the bot so it doesn't reply to itself
+  if (msg.author.bot) return
+  
+  if (msg.content === "$inspire") {
+    getQuote().then(quote => msg.channel.send(quote))
+  }
+});
+
+///process.env is used to access my environment variable
+client.login(process.env.TOKEN);
+
