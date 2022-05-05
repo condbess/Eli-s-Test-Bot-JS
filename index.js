a94c12a46895ae6edf4a06a318ff43e0e9031a09
Elijah <elijah-moore@live.com>
2022-05-04 23:35:36 -0400

    Updated

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 const { Client, Intents } = require("discord.js");
 //fetch will be used to call the API
-const fetch = new Client({ intents: [
-  Intents.FLAGS.NODES,
-  Intents.FLAG.NODE_RES
-]});
-const client = new Client({ intents: [
-  Intents.FLAGS.GUILDS,
-  Intents.FLAGS.GUILD_MESSAGES
-]});
+const fetch = import("node-fetch");
+const client = new Client({
+  intents: [
+    Intents.FLAGS.GUILDS,
+    Intents.FLAGS.GUILD_MESSAGES
+  ]
+});
 
 //this function fetches and returns the quote from the provided URL
 function getQuote() {
@@ -21,16 +20,26 @@ function getQuote() {
       return data[0]["q"] + " -" + data[0]["a"]
       //q is for quote & a is for author
     })
-}; 
+};
 
 client.on("ready", () => {
   console.log('Logged in as ' + client.user.tag + '!')
 });
 
 client.on("message", msg => {
+
+});
+
+client.on("message", msg => {
+
+  //rite of passage from C... it's enough to make a grown man cry
+  if (msg.content === "Hello") {
+    msg.reply("World!")
+  }
+
   //check if the message is from the bot so it doesn't reply to itself
   if (msg.author.bot) return
-  
+
   if (msg.content === "$inspire") {
     getQuote().then(quote => msg.channel.send(quote))
   }
