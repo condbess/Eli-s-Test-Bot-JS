f7ec891b19165eeedff9e7a1e486d018aac12585
Elijah Moore <101022928+condbess@users.noreply.github.com>
2022-05-08 03:00:02 +0000

    Added a new feature to the README.md and attempted to configure the code for the feature's functionality... will try again tomorrow

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-### Eli-s-Test-Bot-JS<sub>(MAY SOON BE KNOWN AS IGNITE)</sub>
+## Eli-s-Test-Bot-JS<sub>(may soon be known as ignite)</sub>
 Work in progress: The code for a bot that replies with encouraging words to Discord Chat users that say something negative.
 
-When user types '+bestow' the bot will get a random inspirational quote from a relevant API and send to the chat for the user to see.
+### Functionality
+> - When user types '+bestow' the bot will get a random inspirational quote from a relevant API and send to the chat for the user to see. Thus, bestowing positive insight upon the user:relieved:
+> - User should be able to add custom messages of encouragement for the bot to use, when it detects a sad word, directly from a discord chat.
--- index.mjs
+++ index.mjs
@@ -1,11 +1,12 @@
 import { Client, Intents} from 'discord.js'
+import fetch from 'node-fetch' //fetch will be used to call the API
+import Database from '@replit/database'
+
+//print statements are used to verify some imports
 console.log(Client);
-
-//fetch will be used to call the API
-import fetch from 'node-fetch';
-
 console.log(fetch);
 
+const db = new Database();
 const client = new Client({ 
   intents: [
     Intents.FLAGS.GUILDS,
@@ -13,18 +14,45 @@ const client = new Client({
   ]
 }); 
 
+//array of sad words
 const sad_words = [
-  "sad", "depressed", "unhappy", "angry", "dispirited", "dejected", "glum", "gloomy", "desolate", "down"
+  "sad", "depressed", "unhappy", "angry", "dispirited", "dejected", "glum", "gloomy", "desolate", "down", "test"
 ];
 
-const encours = [
+//array containing the frist encouragements
+const initialEncours = [
   "Cheer up!",
   "Hang in there!",
   "You are a great person/bot!"
 ];
 
+//replit database initialization and addition of the initialEncours to the db
+db.get("encours").then(encours => {
+  if (!encours || encours.length === 0) {
+    db.set("encours", initialEncours)
+  }
+});
+
+//this function adds the custom messages to the db
+function updateEncours(encouringMsg) {
+  db.get("encours").then(encours => {
+    encours.push([encouringMsg])
+    db.set("encours", encours)
+  })
+};
+
+//this fuction deletes messages from the db
+function deleteEncours(index) {
+  db.get("encours").then(encours => {
+    if (encours.length > index) { //check index validity
+      encours.splice(index, 1)    //remove 1 item fro the provided index
+      db.set("encours", encours)  //set db after msg deletion
+    }
+  })
+};
+
 //this function fetches and returns the quote from the provided URL
-function getQuote() {
+async function getQuote() {
   return fetch("https://zenquotes.io/api/random")
     //after the info is fetched, 
     //asynchronous call (which allows the function to wait until the data is retrieved "then" it's going to do smth w/ that data)
@@ -54,10 +82,28 @@ client.on("message", msg => {
   if (msg.content === "+bestow") {
     getQuote().then(quote => msg.channel.send(quote))
   }
-
+  
+  //if user text contains at least one sad word from the sad_words array, then reply w/ a randomly selected encour from the encour array
   if (sad_words.some(word => msg.content.includes(word))) {
+    db.get("encours").then(encours => {
     const encour = encours [Math.floor(Math.random() * encours.length)]
-    msg.reply(encour)
+    msg.reply(encour)  
+    })
+  }
+
+  //'new' command for user to add an encouraging message
+  if(msg.content.startsWith("+new")) {
+    const encouringMsg = msg.content.split("+new ")[1] //split string method separates the message from the command string
+    updateEncours(encouringMsg)
+    msg.channel.send("New encouraging message added:thumbsup:")
+  }
+
+  //'del' command for user to add an encouraging message
+  if(msg.content.startsWith("+del")) {
+    const index = parseInt(msg.content.split("+del ")[1])
+    const string = msg.content.split("+del ")[1]
+    deleteEncours(index)
+    msg.channel.send("New encouraging message, \"" + string + "\" deleted:pensive:")
   }
 });
 
